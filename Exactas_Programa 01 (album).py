7b97ca66bec750cb5a6e7a7aec4917f203e23445
Gustavo Ariel Retamar <121575510+gustavoarielretamar@users.noreply.github.com>
2023-03-12 22:23:32 -0300

    Update Exactas_Programa 01 (album).py

--- Exactas_Programa 01 (album).py
+++ Exactas_Programa 01 (album).py
@@ -1,24 +1,30 @@
 # %% LIBRERIAS:
 import random
+
+
 # %% CUANTAS_FIGUS, cantidad de figuritas para llenar un album:
-def cuantas_figus(figus_total, debug = False):
-    album = [0]*figus_total
+def cuantas_figus(figus_total, debug=False):
+    album = [0] * figus_total
     cont = 0
-    while sum (album) < figus_total:
+    while sum(album) < figus_total:
         figu = random.randint(0, figus_total - 1)
         cont += 1
-        album[figu]= 1
+        album[figu] = 1
     if debug:
         print(cont)
     return cont
+
+
 # %% PROMEDIO, promediar una lista:
-def promedio (lista, debug = False):
-    prom = sum(lista)/len(lista)
+def promedio(lista, debug=False):
+    prom = sum(lista) / len(lista)
     if debug:
-        print (prom)
+        print(prom)
     return prom
+
+
 # %% SIMULAR_MUCHAS_REPETICIONES, llena n cantidad de albumes:
-def simular_muchas_repeticiones(n_rep, figus_total, debug = False):
+def simular_muchas_repeticiones(n_rep, figus_total, debug=False):
     n_resultados = [0] * n_rep
     cont = 0
     while cont < n_rep:
@@ -29,35 +35,41 @@ def simular_muchas_repeticiones(n_rep, figus_total, debug = False):
     if debug:
         print(n_resultados)
     return n_resultados
+
+
 # %% DAME_ CHANCES, calcular probabilidad:
-def dame_chance(resultados, cantidad_maxima, debug = False):
+def dame_chance(resultados, cantidad_maxima, debug=False):
     i = 0
     meSirve = 0
-    while i<len(resultados):
+    while i < len(resultados):
         if resultados[i] <= cantidad_maxima:
             meSirve = meSirve + 1
         i += 1
-    chances = meSirve/len(resultados)
+    chances = meSirve / len(resultados)
     if debug:
-        print (chances)
+        print(chances)
     return chances
+
+
 # %% DALE_COMPRAME, cuantas figuritas hay que comprar con PROB
-def dale_comprame (resultados, figus_total, prob, debug = False):
+def dale_comprame(resultados, figus_total, prob, debug=False):
     cont = figus_total
     probabilidad = 0
     while probabilidad < prob:
         probababilidad = dame_chance(resultados, cont)
         cont += 1
     if debug:
-        print (cont)
+        print(cont)
     return cont
+
+
 # %% PARAMETROS:
 figus_total = 6
 n_rep = 1000
 resultados = simular_muchas_repeticiones(n_rep, figus_total)
 cantidad_maxima = 15
-prob= 0.9 #numero entre 0 y 1
+prob = 0.9  # numero entre 0 y 1
 # %% LLAMADAS:
 # simular_muchas_repeticiones(repeticiones, figus)
 # dame_chance(resultados, cantidad_maxima, True)
-dale_comprame (resultados, figus_total, prob, True)
+dale_comprame(resultados, figus_total, prob, True)
